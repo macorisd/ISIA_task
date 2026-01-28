031d91c6b34b3f612c5c1372ed7b9aa86155bf67
Macorís Decena Giménez <macorisd@gmail.com>
2026-01-28 12:52:01 +0100

    Funcion para multiplicar dos matrices

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -46,7 +46,26 @@ public class Matriz {
             } 
         } 
         return matrizResultante; 
-    } 
+    }
+
+    public static Matriz multiplicarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles {
+        if(a.getDimension().width != b.getDimension().height) throw new DimensionesIncompatibles("La multiplicación de matrices requiere que el número de columnas de A sea igual al número de filas de B");
+        int filasA = a.getDimension().height;
+        int columnasA = a.getDimension().width;
+        int columnasB = b.getDimension().width;
+        Matriz matrizResultante = new Matriz(filasA, columnasB, false);
+        
+        for (int i = 0; i < filasA; i++) {
+            for (int j = 0; j < columnasB; j++) {
+                int suma = 0;
+                for (int k = 0; k < columnasA; k++) {
+                    suma += a.datos[k][i] * b.datos[j][k];
+                }
+                matrizResultante.datos[j][i] = suma;
+            }
+        }
+        return matrizResultante;
+    }
 
     @Override
     public String toString(){
